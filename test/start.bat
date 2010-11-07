400f91f8ff7d58aead33bc7d816363ccd9e93e56
Malcolm Spiteri <malcolmspiterionline@gmail.com>
2010-11-07 21:09:13 +0000

    Some path and drive changes

--- test/start.bat
+++ test/start.bat
@@ -1,23 +1,23 @@
 @ECHO OFF
 
 @ECHO Creating virtual floppy image
-vfd OPEN B: C:\Projects\MeliteOS\test\img\floppy.img /NEW /W /144 /F
-vfd FORMAT 1
-rem format B: /A:512 /Q /FS:FAT /V:test
+vfd OPEN A: C:\Users\malcolm\Projects\MeliteOS\test\img\floppy.img /NEW /W /144 /F
+vfd FORMAT 0 
+rem format A: /A:512 /Q /FS:FAT /V:test
 
 @ECHO Copying boot.bin to floppy image
-copy C:\Projects\MeliteOS\build\loader\stage_one\stgone.bin .
-partcopy stgone.bin 0 200 -f1
+copy C:\Users\malcolm\Projects\MeliteOS\build\loader\stage_one\stgone.bin .
+partcopy stgone.bin 0 200 -f0
 del .\stgone.bin
 
 @ECHO Copying MELOADER.SYS to floppy image
-copy C:\Projects\MeliteOS\build\loader\stage_two\stgtwo.bin B:\MELOADER.SYS
+copy C:\Users\malcolm\Projects\MeliteOS\build\loader\stage_two\stgtwo.bin A:\MELOADER.SYS
 
 @ECHO Copying KERNEL.EXE to floppy image
-copy C:\Projects\MeliteOS\build\kernel\kernel.exe B:\KERNEL.EXE
+copy C:\Users\malcolm\Projects\MeliteOS\build\kernel\kernel.exe A:\KERNEL.EXE
 
 @ECHO Closing virtual floppy image
-vfd CLOSE B:
+vfd CLOSE A:
 
 @ECHO Invoking VM
-start C:\Projects\MeliteOS\test\bochsrc.bxrc
+start C:\Users\malcolm\Projects\MeliteOS\test\bochsrc.bxrc
